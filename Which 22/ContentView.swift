3fa847af9df2eeb4b0f16a6e67b7696590a06029
Shaun Hubbard <shaunhubbard2013@icloud.com>
2021-09-17 13:13:22 -0400

    Initial Commit

---
+++ Which 22/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Which 22
+//
+//  Created by shaun on 9/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Which 22/Which_22App.swift
@@ -0,0 +1,17 @@
+//
+//  Which_22App.swift
+//  Which 22
+//
+//  Created by shaun on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Which_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
